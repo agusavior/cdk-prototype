40f16c39853390d63fb34c8e1e95575801b8022b
Agustín Javier Orlando <agusavior@gmail.com>
2021-12-15 01:23:24 +0100

    Added a TODO

--- infrastructure/build.py
+++ infrastructure/build.py
@@ -17,6 +17,8 @@ run('mkdir build')
 
 # Make app.zip
 run('zip -r app.zip .', cwd='./../eb-application')
+# TODO: Use a different name on the app.zip because the name of the zip has to cotain some unique value, like
+# the version of the app. Read more about it on: https://stackoverflow.com/questions/51155927/during-an-aborted-deployment-some-instances-may-have-deployed-the-new-applicati/51833138
 run('mv ./../eb-application/app.zip ./build/')
 
 logging.info('Done.\n Now you can run `cdk deploy` if you want.')
