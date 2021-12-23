5dbca7215e01578e5a6f96db60fa30269e683b5f
Agustín Javier Orlando <agusavior@gmail.com>
2021-12-23 14:50:55 +0100

    Better constants

--- example-processing-app/constants.py
+++ example-processing-app/constants.py
@@ -1,15 +1,16 @@
-from util.constantbuilders import from_env, from_json_file, from_uuid
+from util.constantbuilders import from_env, from_required_env, from_json_file, from_uuid
+
+# Process unique number
+PROCESS_UUID =  from_uuid()
 
 # Amazon metadata information. Read more about it on https://docs.aws.amazon.com/AmazonECS/latest/developerguide/container-metadata.html
 ECS_ENABLE_CONTAINER_METADATA = from_env('ECS_ENABLE_CONTAINER_METADATA')
 ECS_CONTAINER_METADATA_FILE = from_env('ECS_CONTAINER_METADATA_FILE')
 ECS_METADATA_DICT = from_json_file(ECS_CONTAINER_METADATA_FILE) if ECS_CONTAINER_METADATA_FILE else None
 
-PROCESS_UUID = from_uuid()
+AMQP_URL =      from_required_env('AMQP_URL')
+AMQP_RECV_Q =   from_required_env('AMQP_RECV_Q', default_value='processing.pending.1')
+AMQP_SEND_Q =   from_required_env('AMQP_SEND_Q', default_value='processing.done.1')
 
-AMQP_URL = from_env('AMQP_URL', required=True)
-AMQP_RECV_Q = 'processing.pending.1'
-AMQP_SEND_Q = 'processing.done.1'
-
-AMQP_EXCHANGE = 'processing'
-AMQP_ROUTING_KEY = 'processing.#'
+AMQP_EXCHANGE =     'processing'
+AMQP_ROUTING_KEY =  'processing.#'
--- example-processing-app/util/constantbuilders.py
+++ example-processing-app/util/constantbuilders.py
@@ -6,10 +6,15 @@ import json
 
 # If the variable `enviroment_variable_name` is defined as an enviroment variable or if it is defined
 # in your env.py file, return it. Else, return None.
-def from_env(enviroment_variable_name, required = False, default_value = None) -> Optional[str]:
-    value = os.getenv(enviroment_variable_name) or getattr(env, enviroment_variable_name, None) or default_value
+def from_env(enviroment_variable_name, default_value = None) -> Optional[str]:
+    return os.getenv(enviroment_variable_name) or getattr(env, enviroment_variable_name, None) or default_value
     if required and value is None: raise NotImplementedError(f'Please define your {enviroment_variable_name} variable.')
     else: return value
+
+def from_required_env(enviroment_variable_name, default_value = str) -> str:
+    value = from_env(enviroment_variable_name, default_value=default_value)
+    if value is None: raise NotImplementedError(f'Please define your {enviroment_variable_name} variable.')
+    return value
 
 # From a file path, open that file, reads the JSON and convert it to a python dict.
 # This assume that the JSON has an object as root (neither a number nor a string).
