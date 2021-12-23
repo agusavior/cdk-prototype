a96bfb2ab09deff9dcfa740b702ba053813aafeb
Agustín Javier Orlando <agusavior@gmail.com>
2021-12-23 14:50:47 +0100

    fakeprocess more dummy

--- example-processing-app/util/fakeprocess.py
+++ example-processing-app/util/fakeprocess.py
@@ -5,5 +5,5 @@ def fake_process(seconds: int):
     start_time = time()
     a = 0
     while time() - start_time < seconds:
-        a += 1
-        a -= 1
+        a += 1              # Dummy code.
+        if a == 0: break    # Dummy code.
